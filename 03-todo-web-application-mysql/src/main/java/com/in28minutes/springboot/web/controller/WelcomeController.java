927978fee2b24809b78071da43517612c3abbda6
bbnsdevelop <bbnsdevelop@gmail.com>
2020-07-05 15:24:42 -0300

    criando image no docker-compose e alterando informações do micro serviço

--- 03-todo-web-application-mysql/src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
+++ 03-todo-web-application-mysql/src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
@@ -13,6 +13,7 @@ public class WelcomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String showWelcomePage(ModelMap model) {
 		model.put("name", getLoggedinUserName());
+		System.out.println("passou aqui");
 		return "welcome";
 	}
 
