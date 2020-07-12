0d12a0505a571b6bd30ae2cbfb26418e9a448568
Bruno Batista <bbnsdevelop@gmail.com>
2020-07-11 23:25:25 -0300

    finalizando estudos de micro serviço com docker

--- 05-microservices/currency-exchange-service/src/main/java/com/bbnsdevelop/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationH2.java
+++ 05-microservices/currency-exchange-service/src/main/java/com/bbnsdevelop/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationH2.java
@@ -2,9 +2,10 @@ package com.bbnsdevelop.microservices.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplicationH2 {
 
 	public static void main(String[] args) {
